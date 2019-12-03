92a8856e4cf8ffcca5f6c88706c09732b8648674
arnavsurve <arnavsurve@gmail.com>
2019-12-02 18:21:59 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+os.system("python3 /Users/arnavsurve/Documents/compsci/Python/dilbert_scraper/scrape.py")
+# os.system("python3 /Users/arnavsurve/Documents/compsci/Python/dilbert_scraper/twitter.py")
